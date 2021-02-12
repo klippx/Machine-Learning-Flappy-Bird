06566e02a6bf79e059e37a1f76853dd7aa20f750
Mathias Klippinge <mathias.klippinge@gmail.com>
2021-02-12 14:13:17 +0100

    Convert genetic.ts to TS

--- source/src/app.ts
+++ source/src/app.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import path from "path";
+import express from 'express'
+import path from 'path'
 
-const app = express();
+const app = express()
 
-app.use(express.static("./"));
+app.use(express.static('./'))
 // FIXME: Dont serve everything
 // app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 app.listen(8080, () => {
-  console.log("App listening on http://localhost:8080");
-});
+  console.log('App listening on http://localhost:8080')
+})
